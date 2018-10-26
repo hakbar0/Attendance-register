fa0802cdb600f7326caffe2ad60df2730c541edf
Haseeb <haseeb2000@hotmail.com>
2018-10-26 14:25:46 +0100

    able to submit properly to firebase. Also made code more modular.

--- attendance-register/src/App.js
+++ attendance-register/src/App.js
@@ -1,46 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
-import moment from 'moment';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import AttendanceForm from './components/AttendanceFrom';
 
 class App extends Component {
-
-  state = {
-    firstname: "",
-    surname: "",
-    notes: "",
-    date: "",
-    time: ""
-  }
-
-  handleSubmit() {
-    //todo push to db
-  }
-
-  handleChange(event, type) {
-    event.preventDefault();
-    let formData = event.target.value;
-
-    if (type === 0) this.setState({ firstname: formData });
-    else if (type === 1) this.setState({ surname: formData });
-    else this.setState({ notes: formData });
-
-    this.setState({ date: moment().format('dddd, MMMM Do YYYY') });
-    this.setState({ time: moment().format('LT') });
-  }
-
   render() {
     return (
-      <div className="App">
-        <form onSubmit={this.handleSubmit}>
-          First name: <br />
-          <input type="text" onChange={event => this.handleChange(event, 0)} className="firstname" /><br />
-          Surname: <br />
-          <input type="text" onChange={event => this.handleChange(event, 1)} className="surname" /><br />
-          Notes: <br />
-          <input type="input" name="notes" onChange={event => this.handleChange(event, 2)} className="notes" /><br />
-          <input type="submit" value="Submit" className="submit" />
-        </form>
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/attendance-form" component={AttendanceForm} />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
---
+++ attendance-register/src/components/AttendanceFrom.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import '../App.css';
+import moment from 'moment';
+import db from '../firebase';
+
+class AttendanceForm extends Component {
+
+    state = {
+        firstname: "",
+        surname: "",
+        notes: ""
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+
+        db.ref('/Users').push({
+            firstname: this.state.firstname,
+            surname: this.state.surname,
+            notes: this.state.notes,
+            date: moment().format('dddd, MMMM Do YYYY'),
+            time: moment().format('LT')
+        })
+    }
+
+    handleChange(event, type) {
+        event.preventDefault();
+        let formData = event.target.value;
+
+        if (type === 0) this.setState({ firstname: formData });
+        else if (type === 1) this.setState({ surname: formData });
+        else this.setState({ notes: formData });
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <form onSubmit={this.handleSubmit.bind(this)}>
+                    First name: <br />
+                    <input type="text" onChange={event => this.handleChange(event, 0)} className="firstname" /><br />
+                    Surname: <br />
+                    <input type="text" onChange={event => this.handleChange(event, 1)} className="surname" /><br />
+                    Notes: <br />
+                    <input type="input" name="notes" onChange={event => this.handleChange(event, 2)} className="notes" /><br />
+                    <input type="submit" value="Submit" className="submit" />
+                </form>
+            </div>
+        );
+    }
+}
+
+export default AttendanceForm;
